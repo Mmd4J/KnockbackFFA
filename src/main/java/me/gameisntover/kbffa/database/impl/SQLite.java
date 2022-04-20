e74f9ca1aa2245e9a71c79e175c2d1adf0467283
Ahmed Waleed <61851106+iiAhmedYT@users.noreply.github.com>
2022-04-19 20:10:12 -0700

    Make SQLite almost functioning

--- src/main/java/me/gameisntover/kbffa/database/impl/SQLite.java
+++ src/main/java/me/gameisntover/kbffa/database/impl/SQLite.java
@@ -23,23 +23,28 @@ public class SQLite implements Database {
             }
         }
         url = "jdbc:sqlite:" + file.getPath();
-        //TODO: create kbffa table
+        createTables();
     }
 
     @Override
     public ReworkedKnocker getKnocker(UUID uuid) {
         String sql = "SELECT * FROM kbffa WHERE uuid=" + uuid;
-        try {
-            Connection connection = createConnection();
+        ReworkedKnocker knocker = new ReworkedKnocker(uuid);
+        try (Connection connection = createConnection()) {
             Statement statement = connection.createStatement();
-            ResultSet set = statement.executeQuery(sql);
-            if(set.first()){
-                //TODO: get the knocker
+            ResultSet result = statement.executeQuery(sql);
+            if(result.first()){
+                knocker.setKills(result.getInt("kills"));
+                knocker.setDeaths(result.getInt("deaths"));
+                knocker.setBalance(result.getInt("balance"));
+                knocker.setSelectedCosmetic(result.getString("selectedCosmetic"));
+                knocker.setSelectedTrail(result.getString("selectedTrail"));
+                knocker.setSelectedKit(result.getString("selectedKit"));
             }
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
-        return null;
+        return knocker;
     }
 
     private Connection createConnection(){
@@ -51,5 +56,27 @@ public class SQLite implements Database {
         }
         return connection;
     }
+
+    private void createTables(){
+        String sql = "CREATE TABLE IF NOT EXIST kbffa (\n" +
+                "uuid text PRIMARY KEY,\n" +
+                "name text NOT NULL,\n" +
+                "kills integer NOT NULL DEFAULT 0,\n" +
+                "deaths integer NOT NULL DEFAULT 0,\n" +
+                "maxkillstreak integer NOT NULL DEFAULT 0,\n" +
+                "balance integer NOT NULL DEFAULT 0,\n" +
+                "selectedCosmetic text NOT NULL,\n" +
+                "selectedTrail text NOT NULL,\n" +
+                "selectedKit text NOT NULL,\n" +
+                ");";
+
+        try (Connection connection = createConnection()){
+            Statement statement = connection.createStatement();
+            statement.execute(sql);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 
 }
